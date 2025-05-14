229a372429ffc0193ff64cbf45fbf884b55c1d6b
ManuelBenavente999 <manbegon1994@gmail.com>
2025-05-14 14:04:45 +0200

    Comentarios Descriptivos de Registro.php

--- yestelle/registro.php
+++ yestelle/registro.php
@@ -1,6 +1,7 @@
 <?php
 include 'conexion.php';
 
+// Comprobacion de datos añadidos con exito o error
 if (
   empty($_POST['usuario']) || empty($_POST['contrasena']) ||
   empty($_POST['nombre']) || empty($_POST['apellidos']) ||
@@ -10,6 +11,7 @@ if (
   exit;
 }
 
+// Envio de datos a la base de datos mediante POST
 $usuario = $_POST['usuario'];
 $contrasena = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
 $nombre = $_POST['nombre'];
@@ -18,14 +20,17 @@ $correo = $_POST['correo'];
 $fecha = $_POST['fecha'];
 $genero = $_POST['genero'];
 
+// Insercion de datos en la tabla USUARIOS de "usuario" y "contrasena"
 $stmt1 = $conn->prepare("INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)");
 $stmt1->bind_param("ss", $usuario, $contrasena);
 
+// Insercion de datos en la tabla CLIENTES de todos los datos
 if ($stmt1->execute()) {
   $stmt2 = $conn->prepare("INSERT INTO clientes (nombre, apellidos, correo, fecha_nacimiento, genero) VALUES (?, ?, ?, ?, ?)");
   $stmt2->bind_param("sssss", $nombre, $apellidos, $correo, $fecha, $genero);
   $stmt2->execute();
 
+  // Mensajes de Exito y Error
   echo "<script>alert('Registro exitoso'); window.location.href='index.html';</script>";
 } else {
   echo "<script>alert('El usuario ya existe'); window.location.href='registro.html';</script>";
